329aed82dc0badf94a144ee969803aa30ece878f
Marcelo Cardoso <devmarcelocardoso@gmail.com>
2023-03-30 21:15:58 -0300

    feat: wait template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import logo from './style/images/logo.svg'
+
 function App() {
   return (
-    <>
-      <h1>Aplicação ReactJS</h1>
-    </>
+    <div className='m-4 font-doppio grid grid-rows-2 items-between'>
+      <>
+        <img src={logo} alt="Suden Representações LTDA"/>
+      </>
+      <>
+        <>
+          <b className='text-5xl text-black-700'>
+            <p>Seja bem vindo a</p>
+            <p>Suden Representações</p>
+          </b>
+        </>
+        <p className='text-3xl'>Nosso novo site está em construção</p>
+      </>
+    </div>
   );
 }
 
