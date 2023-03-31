64795231d05551bc181ab0f7583155d82c13ed83
Marcelo Cardoso <devmarcelocardoso@gmail.com>
2023-03-31 13:41:02 -0300

    style: add environment tag

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import logo from './style/images/logo.svg'
 
 function App() {
   return (
-    <div className='m-4 font-doppio grid grid-rows-2 items-between'>
-      <>
-        <img src={logo} alt="Suden Representações LTDA"/>
-      </>
+    <div className='m-4 font-doppio grid gap-20'>
+      <div className='flex justify-between items-center'>
+        <span><img src={logo} alt="Suden Representações LTDA"/></span>
+        <span className='bg-green-500 p-2'>Ambiente de homologação</span>
+      </div>
       <>
         <>
-          <b className='text-5xl text-black-700'>
-            <p>Seja bem vindo a</p>
-            <p>Suden Representações</p>
-          </b>
+          <span className='text-5xl text-black-700'>
+            <b>
+              <p>Seja bem vindo a</p>
+              <p>Suden Representações</p>
+            </b>
+            <p className='text-3xl text-black'>Nosso novo site está em construção</p>
+          </span>
         </>
-        <p className='text-3xl'>Nosso novo site está em construção</p>
       </>
     </div>
   );
