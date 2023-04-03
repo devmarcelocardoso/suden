32ceac4d31a127bf24de0fea8480c842a325fd9b
Marcelo Cardoso <devmarcelocardoso@gmail.com>
2023-04-02 22:57:06 -0300

    feat: main file

---
+++ src/Main.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/header";
+
+function Main(props){
+  return(
+    <main className='mx-2.5 my-1.5 sm:mx-10 sm:my-5 font-doppio'>
+      <Header/>
+    </main>
+  )
+}
+
+export default Main;
