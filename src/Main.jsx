a823d301a9f684e0928d7b0471e06ad29e632b58
Marcelo Cardoso <devmarcelocardoso@gmail.com>
2023-04-04 13:54:34 -0300

    feat: add components

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,20 @@
 import Header from "./components/header";
+import Home from "./components/home";
+import Partners from "./components/partners";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
 
 function Main(props){
   return(
-    <main className='mx-2.5 my-1.5 sm:mx-10 sm:my-5 font-doppio'>
-      <Header/>
-    </main>
+    <>
+      <main className='mx-2.5 my-1.5 sm:mx-10 sm:my-5 space-y-12'>
+        <Header/>
+        <Home/>
+        <Partners/>
+        <Contact/>
+      </main>
+      <Footer/>
+    </>
   )
 }
 
