118e06fed09336c6aca3c06e238d0733f76ca0e4
Marcelo Cardoso <devmarcelocardoso@gmail.com>
2023-04-04 13:53:06 -0300

    fix: add transition

--- src/components/buttons/secondary.jsx
+++ src/components/buttons/secondary.jsx
@@ -1,7 +1,7 @@
 function Secondary(props){
   return(
     <span className={props.className}>
-      <button className='hidden sm:block py-2 px-4 border text-lg'>{props.value}</button>
+      <button className='hidden sm:block py-2 px-4 text-sm transition duration-500 hover:ease-out'>{props.value}</button>
     </span>
   )
 }
