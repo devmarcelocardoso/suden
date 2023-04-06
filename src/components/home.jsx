a21d6ac2e2300d3e42d00b69de9fe3351346d063
Marcelo Cardoso <devmarcelocardoso@gmail.com>
2023-04-05 21:43:55 -0300

    fix: in home use tabswitcher to each segment

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,22 +1,22 @@
 import { React } from 'react';
 import homeBg from '../style/images/home-bg.svg'
-import Index from './segments/index';
 import Primary from '../components/buttons/primary'
+import Segments from './segments/index';
 
 function Home(){
   return(
     <>
-      <section className='flex justify-between gap-4'>
+      <section id='home' className='h-fit flex justify-between gap-4'>
         <>
-          <main className='flex flex-col gap-6 w-[616px]'>
+          <main className='flex flex-col justify-center gap-6 w-[620px]'>
             <h1 className='sm:text-6xl text-4xl text-green font-bold'>Lorem ipsum dolor</h1>
             <p className='sm:text-gl text-base text-black'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pellentesque placerat purus ac mollis. Etiam iaculis auctor dui sed tincidunt. Ut vel dolor erat. Praesent imperdiet vehicula congue. In suscipit Nunc aliquet efficitur lacus, sed malesuada nibh fringilla eget. Suspendisse.</p>
           </main>
         </>
         <img src={homeBg} className='w-64 sm:block hidden' alt='World network'/>
       </section>
-      <section className='sm:flex justify-between items-center'>
-        <Index/>
+      <section id='segments' className='sm:flex justify-between items-start'>
+        <Segments/>
         <a href='https://wa.me/5551999673527' target='blank' className='sm:block hidden'>
           <Primary value='Faça parte da Suden'/>
         </a>
