626c95ec5e8788b0f048604560c13866682e4cc6
Marcelo Cardoso <devmarcelocardoso@gmail.com>
2023-04-05 21:45:59 -0300

    style: resizing space-y

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,13 +1,13 @@
 import Header from "./components/header";
 import Home from "./components/home";
-import Partners from "./components/partners";
+import Partners from "./components//partners/index";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
 
-function Main(props){
+function Main(){
   return(
     <>
-      <main className='mx-2.5 my-1.5 sm:mx-10 sm:my-5 space-y-12'>
+      <main className='mx-2.5 my-1.5 sm:mx-10 sm:my-5 space-y-6'>
         <Header/>
         <Home/>
         <Partners/>
